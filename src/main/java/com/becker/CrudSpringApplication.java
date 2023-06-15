38224b26b63503ffb24bfb62168bc6dfc9b9fa11
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-15 18:45:56 -0300

    deploy api

---
+++ src/main/java/com/becker/CrudSpringApplication.java
@@ -0,0 +1,16 @@
+package com.becker;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+}
+
